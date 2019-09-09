714ffe2411d59d9b2531c240d4607512bd6548bc
Gisle Halvorsen <Gislefh@stud.ntnu.no>
2019-09-09 16:19:02 +0200

    added suport for validation data in the  same folder

--- classes/class_generator.py
+++ classes/class_generator.py
@@ -20,7 +20,7 @@ import requests
 class Generator:
 
 
-	def __init__(self, path, X_shape, Y_shape, N_classes, N_channels, batch_size):
+	def __init__(self, path, X_shape, Y_shape, N_classes, N_channels, batch_size, train_val_split = 0.3, class_list = []):
 
 		self.path = path 				#path to folder, str
 		self.X_shape = X_shape			#shape of output, (samples, x, y, channel) or (samples, x, y, z, channels)
@@ -31,64 +31,131 @@ class Generator:
 		self.image = None
 		self.aug_method = []
 		self.aug_args = []
+		self.train_val_split = train_val_split 	#defaults to 0.3
+
+		if class_list:
+			self.class_list = class_list
+		else:
+			self.class_list = []
+
+	#returns the classes (folders) in the directory
+	def get_classes(self):
+		if self.class_list:
+			return self.class_list
+		else:
+			class_list = []
+			for folder in os.listdir(self.path):
+				class_list.append(folder)
+			return class_list
+
+	# returns the chosen augmentations. Should be called after augmentations
+	def get_aug(self):
+		augs = []
+		for aug in self.aug_method:
+			augs.append(aug.__name__)
+		return augs
+	
+
+	## works for all the data in the folders
+	def get_length_data(self):
+		N_data = 0
+		for folder in os.listdir(self.path):
+			if self.class_list:
+				if folder in self.class_list:
+					for file_ in os.listdir(self.path + '\\' +folder):
+						N_data  = N_data + 1
+			else:
+				for file_ in os.listdir(self.path + '\\' +folder):
+					N_data  = N_data + 1					
+		return N_data
 
 
-	def __im_reshape(self, orig_shape, image):
+	''' saves a list of derectories to images with the image class
+	IN:
+	N_images_per_class: how many images to get per class
+
+	'''
+	def __from_dir(self, N_images_per_class):
+
+		image_list = []
+		class_ = 0
+
+		for folder in os.listdir(self.path):
+			cnt_img_per_class = 0
+			if self.class_list:
+				if folder not in self.class_list:
+					continue
+				
+			for image_ in os.listdir(self.path +'/' +folder):
+				if N_images_per_class != None:
+					if cnt_img_per_class > N_images_per_class:
+						break
+					
+				image_list.append([self.path + '/' + folder +'/' +image_, class_])
+				cnt_img_per_class += 1
+			
+			class_ += 1
+
+		## shuffle and save into test and train
+		## TODO fix so that there are equal images for each class
+		np.random.shuffle(image_list)
+		image_list = np.array(image_list)
+		self.train_set = image_list[0:int(image_list.shape[0] * (1-self.train_val_split)),  :]
+		self.val_set = image_list[int(image_list.shape[0] * (1-self.train_val_split)):-1,  :]
+
+
+
+	''' Creates a generator for either training set or validation set
+	- IN:
+	set: either val, train or test. str
+	N_images_per_class: how many images to get per class
+	train_val_split: how much of the data thats used as validaion
+	'''
+
+	def flow_from_dir(self, set = 'train', N_images_per_class = None, augment_validation = True):
 		
-		factor_x = self.X_shape[1] / orig_shape[0]
-		factor_y = self.X_shape[2] / orig_shape[1]
 
-		return ndimage.zoom(image, (factor_x, factor_y, 1), order = 3)
+		if set == 'test':
+			self.train_val_split = 0
 
-
-	def generator_from_dir(self, include_folder_list = [], N_images_per_class = None):
+		# create sets
+		self.__from_dir(N_images_per_class)
 
 		self.X = np.zeros(self.X_shape)
 		self.Y = np.zeros(self.Y_shape)
 
+		if set == 'train':
+			tot_list = self.train_set
+		elif set == 'val':
+			tot_list = self.val_set
+		elif set == 'test':
+			tot_list = self.train_set
+		else:
+			print("select either: 'train', 'val' or 'test'")
+			exit()
 
 		while True:
 
-			image_list = []
-			class_ = 0
-
-			for folder in os.listdir(self.path):
-				cnt_img_per_class = 0
-				if include_folder_list:
-					if folder not in include_folder_list:
-						continue
-					
-				for image_ in os.listdir(self.path +'/' +folder):
-					if N_images_per_class != None:
-						if cnt_img_per_class > N_images_per_class:
-							break
-						
-					image_list.append([self.path + '/' + folder +'/' +image_, class_])
-					cnt_img_per_class += 1
-				
-				class_ += 1
-
-			image_list = np.array(image_list)
+			image_list = tot_list.copy()
 
 			for i in range(len(image_list)):
-
 				##choose random image from list
 				choice = np.random.choice(len(image_list[:, 0]))
 				self.image = cv2.imread( image_list[choice, 0])[:, :, 0:self.N_channels]
 				label = int(image_list[choice, 1])
 
-				#norm image to [0,1]
+				#normalize image to [0,1]
 				self.image = np.clip(self.image / 255, 0, 1)
 			
-	
-				### add augmentation
-				for j, aug_method in enumerate(self.aug_method):
-					if self.aug_args[j] == None:
-						aug_method()
-					else:
-						aug_method(self.aug_args[j])
-					
 				
+				### add augmentation	
+				if (set == 'train') or (set == 'val' and augment_validation):
+					for j, aug_method in enumerate(self.aug_method):
+						if self.aug_args[j] == None:
+							aug_method()
+						else:
+							aug_method(self.aug_args[j])
+						
 				## reshape image
 				if self.image.shape != self.X[0].shape:
 					self.X[i%self.batch_size] = self.__im_reshape(self.image.shape, self.image)
@@ -96,8 +163,8 @@ class Generator:
 					self.X[i%self.batch_size] = self.image
 
 				## one hot encode ground truth
-				if include_folder_list:
-					self.Y[i%self.batch_size] = one_hot(label, len(include_folder_list))
+				if self.class_list:
+					self.Y[i%self.batch_size] = one_hot(label, len(self.class_list))
 				else:
 					self.Y[i%self.batch_size] = one_hot(label, self.N_classes)
 
@@ -257,7 +324,7 @@ class Generator:
 
 	#max_abs_angle_deg: maximum angle of rotation. positive scalar
 	def add_rotate(self, max_abs_angle_deg = 10):
-		self.aug_method.append(self.__add_rotate)
+		self.aug_method.append(self.__rotate)
 		self.aug_args.append(max_abs_angle_deg)
 	
 	#min: the lowest value 
@@ -272,24 +339,29 @@ class Generator:
 		self.aug_method.append(self.__shift)
 		self.aug_args.append(max_shift)
 	
-	#axis: for images, axis is 0 or 1 or 'random'. defaults to random
+	#flips image
 	def add_flip(self):
 		self.aug_method.append(self.__flip)
 		self.aug_args.append(None)
+
+	#zooms image in the range zoom_range
+	def add_zoom(self, zoom_range):
+		self.aug_method.append(self.__zoom)
+		self.aug_args.append(zoom_range)		
 
 	#-----
 	def __gamma_transfrom(self, args):
 		gamma = np.random.uniform(args[0], args[1])
 		self.image = np.clip(np.power(self.image, gamma), 0, 1)
 
-	def __add_rotate(self, max_angle):	
+	def __rotate(self, max_angle):	
 		angle = 2 * max_angle * np.random.rand() - max_angle 
 		self.image =  ndimage.rotate(self.image, angle, reshape = False)
 		self.image = np.clip(self.image, 0, 1)
 	
 	def __shift(self, max_shift):
-		shift_x = np.random.uniform(0, self.image.shape[0]) * max_shift
-		shift_y = np.random.uniform(0, self.image.shape[1]) * max_shift
+		shift_x = np.random.uniform(-self.image.shape[0], self.image.shape[0]) * max_shift
+		shift_y = np.random.uniform(-self.image.shape[1], self.image.shape[1]) * max_shift
 		self.image = ndimage.shift(self.image, (shift_x, shift_y, 0))
 		self.image = np.clip(self.image, 0, 1)
 
@@ -298,27 +370,28 @@ class Generator:
 			self.image = np.flip(self.image, axis = 1)
 			self.image = np.clip(self.image, 0, 1)
 
+	def __zoom(self, args):
+		if np.random.rand() > 0.5:
+			zoom_range_x = args[0] + (np.random.rand() * (args[1]- args[0]))
+			zoom_range_y = 1
+		else:
+			zoom_range_x = 1
+			zoom_range_y = args[0] + (np.random.rand() * (args[1]- args[0]))
+
+		self.image = ndimage.zoom(self.image, (zoom_range_x, zoom_range_y, 1), order = 3)
 
 
 	
 
-	######## ----
+	######## ---- utils ---
 
+	def __im_reshape(self, orig_shape, image):
+		
+		factor_x = self.X_shape[1] / orig_shape[0]
+		factor_y = self.X_shape[2] / orig_shape[1]
 
-	def steps_per_epoch(self, include_folder_list = []):
-		image_list = []
-		class_ = 0
-		for folder in os.listdir(self.path):
-			if include_folder_list:
-				if folder not in include_folder_list:
-					continue
-				
-			for image_ in os.listdir(self.path +'/' +folder):
-				image_list.append([self.path + '/' + folder +'/' +image_, class_])
-			
-			class_ += 1
+		return ndimage.zoom(image, (factor_x, factor_y, 1), order = 3)
 
-		#image_list = np.array(image_list)	for folder in os.listdir:
 
 	# TODO fix this
 	def generator_from_zip(self):
