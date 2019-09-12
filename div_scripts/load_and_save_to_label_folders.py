f63ca93982755a1caec93141eaaedecfa46653c4
Gisle Halvorsen <Gislefh@stud.ntnu.no>
2019-09-12 09:21:26 +0200

    adding scripts for google colab

--- div_scripts/load_and_save_to_label_folders.py
+++ div_scripts/load_and_save_to_label_folders.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 save_path = 'C:\\Users\\47450\\Documents\\ResQ Biometrics\\Data sets\\ExpW\\'
 data_path = 'C:\\Users\\47450\\Downloads\\ExpW\\origin\\'
-label_path = 'C:\\Users\\47450\\Downloads\\'
+label_path = 'C:\\Users\\47450\\Documents\\ResQ Biometrics\\Data sets\\ExpW_original_images\\'
 
 label_list = list(open(label_path + 'label.lst'))
 
@@ -15,6 +15,16 @@ image_name   face_id_in_image    face_box_top   face_box_left    face_box_right 
 labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 #files = os.listdir(path):
 
+label_tot = []
+for i in range(len(label_list)):
+    label_tot.append(int(label_list[i].split(' ')[-1]))
+    
+
+
+plt.hist(np.array(label_tot),bins = 7)
+plt.grid()
+plt.show()
+exit()
 for im_number, item in enumerate(label_list):
     impath = data_path + item.split(' ')[0]
     label = item.split(' ')[-1]
