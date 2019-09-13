607a7f59c9dd5837076d5417f58c23e5ada42431
Gisle Halvorsen <Gislefh@stud.ntnu.no>
2019-09-13 11:27:30 +0200

    added plot for history

--- div_scripts/plot_history.py
+++ div_scripts/plot_history.py
@@ -1,19 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-path_to_meta_data = 'C:\\Users\\47450\\Documents\\ResQ Biometrics\\ResQ-Biometrics-CNN\\Models\\From_Colab\\meta_data_model_4.npy'
+path_to_meta_data = 'C:\\Users\\47450\\Documents\\ResQ Biometrics\\ResQ-Biometrics-CNN\\Models\\meta_data_model_4.npy'
 
 meta_data = np.load(path_to_meta_data, allow_pickle = True)
 
 history = meta_data.item()['model_history']
 
-print(history.history)
+print(history.history['val_loss'])
 
+epochs = list(range(len(history.history['val_loss'])))
+val_loss = history.history['val_loss']
+loss = history.history['loss']
+val_acc = history.history['val_acc']
+acc = history.history['acc']
 
+plt.figure('Traning summary')
+plt.subplot(211)
+plt.plot(epochs, val_loss)
+plt.plot(epochs, loss)
+plt.legend(['val_loss', 'train_loss'])
+plt.grid()
+plt.xlabel('Epochs')
+plt.ylabel('loss')
 
+plt.subplot(212)
+plt.plot(epochs, val_acc)
+plt.plot(epochs, acc)
+plt.legend(['val_acc', 'train_acc'])
+plt.grid()
+plt.xlabel('Epochs')
+plt.ylabel('accuracy')
 
-
-
+plt.show()
 
 '''
 meta_data = {'model_name' : model_name,
