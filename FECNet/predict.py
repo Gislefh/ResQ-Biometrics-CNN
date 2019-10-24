75cdc41f7c65fc7187a52a325f94bef5152a0e7a
Gisle Halvorsen <Gislefh@stud.ntnu.no>
2019-10-24 12:55:21 +0200

    predictor for FECNet

--- FECNet/predict.py
+++ FECNet/predict.py
@@ -1,61 +1,46 @@
 import tensorflow as tf 
-from generator import TripletTestGenerator
-from model import test_siam_model
-from utils import distances, pca
+from generator import TripletGenerator
+from model import FECNet_inceptionv3_model
+from utils import distances, pca_FECNet, eval_gen
 import numpy as np
 import matplotlib.pyplot as plt
+from custom_loss import TripletLoss
 
+path = 'C:\\Users\\47450\\Documents\\ResQ Biometrics\\Data sets\\FEC_dataset\\images\\two-class_triplets'
 save_model_path = 'C:\\Users\\47450\\Documents\\ResQ Biometrics\\ResQ-Biometrics-CNN\\FECNet\\Models\\'
 new_model_name = 'MNIST_digits_test.h5'
-out_shape = (28,28)
+out_shape = (224, 224, 3)
 embedding_size = 16 # faceNet uses 128, FECNet uses 16.
-batch_size = 8
-
 batch_size = 1
+delta_trip_loss = 0.2
+
+# Data Generator
+trip_gen = TripletGenerator(path, out_shape = out_shape, batch_size=batch_size, augment=True, data=5000)
+gen = trip_gen.flow_from_dir()
+data_len = trip_gen.get_data_len()
 
 
-G = TripletTestGenerator(out_shape, batch_size)
-gen = G.flow()
+# Model
+model = FECNet_inceptionv3_model(input_shape = out_shape, embedding_size = embedding_size)
+model.load_weights('Models\\FECNet_test1.h5')
 
 
-
-#weights = tf.keras.models.load_weights('Models\\MNIST_digits_test.h5')
-
-model = test_siam_model(input_shape=out_shape, embedding_size=embedding_size)
-model.load_weights('Models\\MNIST_digits_test_only_weights.h5')
-
-
-from keras.datasets import mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-
-x = []
-y = []
-for i in range(len(y_test)):
-    y.append(y_test[i])
-    if i%3 == 2:
-        x.append([np.expand_dims(x_test[i-2], axis = 0), np.expand_dims(x_test[i-1], axis = 0), np.expand_dims(x_test[i], axis = 0)])
-        
-
-pred = []
-for i, samp in enumerate(x):
-    pred.append(model.predict(samp, batch_size = batch_size, steps = 1))
-
-pca(pred, embedding_size, y, N_comp=3)
-
-
+eval_gen(gen, model, data_len, embedding_size)
 exit()
-    
 
 
-cnt = 0
 pred = []
-for x,y in gen:
-    pred.append(model.predict(x, batch_size = batch_size, steps = 1))
-    cnt += 1
-    if cnt > 200:
-        pca(pred, embedding_size)
+images = []
+for i, (x,y) in enumerate(gen):
+    if i >= data_len:
         break
+    
+    pred.append(model.predict(x, batch_size = batch_size, steps = 1))
+    print('predicted on ' , i, ' tripets')
+
+pca_FECNet(pred, embedding_size, I, N_comp = 2)
+
+
 
 
 
